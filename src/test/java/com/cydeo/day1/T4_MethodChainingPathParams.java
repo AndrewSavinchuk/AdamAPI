019df8b4f85bc5cdc744bc13bc5cb0a24385b978
CrazyDevo <adam@cybertekschool.com>
2022-04-27 15:59:05 -0400

    Task 4 is ready

---
+++ src/test/java/com/cydeo/day1/T4_MethodChainingPathParams.java
@@ -0,0 +1,36 @@
+package com.cydeo.day1;
+
+import org.junit.jupiter.api.Test;
+
+public class T4_MethodChainingPathParams {
+    /**
+     * Task 4:
+     *
+     * 1. Given accept type is Json
+     * 2. Path Parameters values are
+     *      - id —> 5
+     * 3. When user sends GET request to /spartans/{id}
+     * 4. Verify followings
+     *      - Status code should be 200
+     *      - Content Type is application/json
+     *      - ID is 5
+     *      - Name is "Blythe"
+     *      - gender is "Female"
+     *      - phone is 3677539542
+     */
+
+    @Test
+    public void test(){
+        //1. Given accept type is Json
+        //2. Path Parameters values are
+        //     - id —> 5
+        //3. When user sends GET request to /spartans/{id}
+        //4. Verify followings
+        //     - Status code should be 200
+        //     - Content Type is application/json
+        //     - ID is 5
+        //     - Name is "Blythe"
+        //     - gender is "Female"
+        //     - phone is 3677539542
+    }
+}
