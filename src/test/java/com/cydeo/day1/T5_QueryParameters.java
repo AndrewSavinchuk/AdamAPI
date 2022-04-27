a1fec3c493d6fe1be1b56b52f35b01dc6feea67b
CrazyDevo <adam@cybertekschool.com>
2022-04-27 16:46:48 -0400

    Task 5 is done

---
+++ src/test/java/com/cydeo/day1/T5_QueryParameters.java
@@ -0,0 +1,26 @@
+package com.cydeo.day1;
+
+public class T5_QueryParameters {
+
+    // Use SpartanTestBase instead of keep typing BeforeAll and AfterAll
+
+    /**
+     *1- Given accept type is Json
+     *2- Query Parameters values are
+     *     - gender —> Female
+     *     - nameContains —> e
+     *3- When user sends GET request to /spartans/search
+     *4- Print out Followings
+     *     - Total Element Number
+     *     - Get me first spartan name
+     *     - Get me second spartan id
+     *     - Get me last spartan name
+     *     - Get me all Spartan Names
+
+     * 5- Verify followings
+     *     - Status code should be 200
+     */
+
+
+
+}
